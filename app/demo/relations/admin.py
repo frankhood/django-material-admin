ddf43769b85e39aeac2ef7d9bab200d6c1cbf4f4
skill83 <skill83@gmail.com>
2020-04-03 12:06:49 +0200

    added changes to test issues

--- app/demo/relations/admin.py
+++ app/demo/relations/admin.py
@@ -7,5 +7,6 @@ from demo.relations.models import Relation
 class RelationAdmin(ModelAdmin):
     list_display = ('id', 'name')
     icon_name = 'layers'
-    autocomplete_fields = ('documents', 'user')
+    autocomplete_fields = ('documents',)
     prepopulated_fields = {"slug": ("name",)}
+    raw_id_fields = ('user',)
